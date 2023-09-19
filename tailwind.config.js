415832cddda4fac9c8b870b9d3c9b38624ed4757
aidilfitra08 <aidil.gitu1@gmail.com>
2023-09-19 11:10:06 +0700

    Update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '128' : '32rem',
+      },
+      height: {
+        '128': '32rem',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
+
