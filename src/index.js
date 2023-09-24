23b6d540cd64055ef01de0e58c81c7cfad1dbff7
aidilfitra08 <aidil.gitu1@gmail.com>
2023-09-24 15:21:09 +0700

    update index app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ToastContainer
@@ -28,6 +28,26 @@ root.render(
     />
     <App />
   </React.StrictMode>
+  // <>
+  //   <ToastContainer
+  //     toastClassName={() =>
+  //       "relative flex py-4 px-3 rounded overflow-hidden cursor-pointer bg-white shadow-lg"
+  //     }
+  //     bodyClassName={() => "text-black text-base font-normal"}
+  //     position="bottom-left"
+  //     autoClose={4000}
+  //     hideProgressBar={true}
+  //     newestOnTop={false}
+  //     closeButton={false}
+  //     closeOnClick
+  //     rtl={false}
+  //     pauseOnFocusLoss
+  //     draggable
+  //     pauseOnHover
+  //     theme="light"
+  //   />
+  //   <App />
+  // </>
 );
 
 // If you want to start measuring performance in your app, pass a function
