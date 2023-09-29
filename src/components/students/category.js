8ffc02c17151fc50657f76b11a41f30b85e82ea1
aidilfitra08 <aidil.gitu1@gmail.com>
2023-09-29 15:28:36 +0700

    add category data

---
+++ src/components/students/category.js
@@ -0,0 +1,12 @@
+export const category = [
+  "Marketing Management",
+  "Social Media Marketing",
+  "Creative Copywriting",
+  "Facebook Ads",
+  "TikTok Ads",
+  "Google Ads",
+  "Influencer and Community Marketing",
+  "Search Engine Optimization",
+  "App Store Optimization",
+  "Customer Relationship Management",
+];
