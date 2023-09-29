823a62288fafa4f68c8360904dc4e9cab1b79eb7
aidilfitra08 <aidil.gitu1@gmail.com>
2023-09-29 15:20:40 +0700

    change session to local

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import React from "react";
 
 const Protected = ({ children }) => {
-  if (!sessionStorage.getItem("token")) {
+  if (!localStorage.getItem("user")) {
     return <Navigate to="/login" replace />;
   }
   return children;
