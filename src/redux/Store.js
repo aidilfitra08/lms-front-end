84bf44db453610c45e06353630d6bbfcd2c66f93
aidilfitra08 <aidil.gitu1@gmail.com>
2023-10-02 14:59:30 +0700

    add lecture reducer to store

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,10 +3,12 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { userReducer } from "./Credential/UserReducer";
 import { courseReducer } from "./Student/StudentReducer";
+import { createCourseReducer } from "./Lecture/LectureReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   studentCourse: courseReducer,
+  lectureCreateCourse: createCourseReducer,
 });
 const Store = configureStore({
   reducer: rootReducer,
