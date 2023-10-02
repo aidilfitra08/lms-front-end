be18245631efdabe176d6ac3e67759e8309e52f3
aidilfitra08 <aidil.gitu1@gmail.com>
2023-10-02 14:57:45 +0700

    add protected routes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,54 @@
 import { Navigate } from "react-router-dom";
 import React from "react";
 
-const Protected = ({ children }) => {
+export const Protected = ({ children }) => {
   if (!localStorage.getItem("user")) {
     return <Navigate to="/login" replace />;
   }
+
+  let role = JSON.parse(localStorage.getItem("user")).role;
+  if (role != "student" && role != "admin") {
+    return <Navigate to="/lecture" replace />;
+  } else if (role != "lecture" && role != "admin") {
+    return <Navigate to="/student" replace />;
+  }
   return children;
 };
-export default Protected;
+
+export const ProtectedFromStudent = ({ children }) => {
+  if (!localStorage.getItem("user")) {
+    return <Navigate to="/login" replace />;
+  }
+
+  let role = JSON.parse(localStorage.getItem("user")).role;
+  if (role != "admin") {
+    if (role == "student") {
+      return <Navigate to="/student" replace />;
+    }
+  }
+
+  return children;
+};
+
+export const ProtectedFromLecture = ({ children }) => {
+  if (!localStorage.getItem("user")) {
+    return <Navigate to="/login" replace />;
+  }
+
+  let role = JSON.parse(localStorage.getItem("user")).role;
+  if (role != "admin") {
+    if (role == "lecture") {
+      return <Navigate to="/lecture" replace />;
+    }
+  }
+
+  return children;
+};
+
+export const ProtectedNotFoundPage = ({ children }) => {
+  if (!localStorage.getItem("user")) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
