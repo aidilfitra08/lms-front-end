5d1c2cbdb113a59ceb86a4d49e2aa543a2df0696
aidilfitra08 <aidil.gitu1@gmail.com>
2023-10-02 16:03:56 +0700

    update homepage components

--- src/components/students/Homepage.js
+++ src/components/students/Homepage.js
@@ -1,34 +1,51 @@
-import React from 'react'
-import Sidenav from '../navbar/Sidenav'
-import Footer from '../footer/Footer'
-import CourseCard from './CourseCard'
+import React, { useEffect } from "react";
+import Sidenav from "../navbar/Sidenav";
+import Footer from "../footer/Footer";
+import CourseCard from "./CourseCard";
+import { connect, useDispatch } from "react-redux";
+import { fetchAllCourses } from "../../redux/Student/StudentAction";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function Homepage(props) {
   function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(" ");
   }
-  
-  const testingMap = [1,2,3,4,5];
+
+  // const testingMap = [1, 2, 3, 4, 5];
+  const courses = props.course.allCourses;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllCourses());
+  }, []);
   return (
     <>
       {/* <Sidenav/> */}
-      <div className={classNames(props.sideBarTrigger ? 'pl-64': 'pl-0', 'pt-16 grid grid-cols-12 bg-blue-400')} >
-        <div className='col-span-9 bg-slate-200 grid grid-cols-3'>
-
-          {testingMap.map((number) => (
-            <div className='col-span-1'>
-            <CourseCard />
+      <div
+        className={classNames(
+          props.sideBarTrigger ? "pl-64" : "pl-0",
+          "pt-16 grid grid-cols-12 "
+        )}
+      >
+        <div className="col-span-9 grid grid-cols-3 py-6">
+          {courses.map((course) => (
+            <div className="col-span-1">
+              <CourseCard courseDetail={course} />
             </div>
           ))}
         </div>
-        <div className='col-span-3 bg-blue-500'>
-          right
+        <div className="col-span-3 m-6">
+          <Calendar value={Date.now()} />
         </div>
-
       </div>
       <Footer sideBarTrigger={props.sideBarTrigger} />
     </>
-  )
+  );
 }
+const mapStateToProps = (state) => {
+  return {
+    course: state.studentCourse,
+  };
+};
 
-export default Homepage
+export default connect(mapStateToProps)(Homepage);
