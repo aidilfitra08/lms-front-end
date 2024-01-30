c8d2842cc352ed14099aec3092e5d621ec964db7
aidilfitra08 <aidil.gitu1@gmail.com>
2024-01-31 00:03:54 +0700

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,7 +12,7 @@ function Footer(props) {
         " bg-indigo-900 bottom-0 h-52 text-neutral-100"
       )}
     >
-      <div className="grid grid-cols-2 py-6 px-10">
+      <div className="grid grid-cols-2 py-6 px-10 mt-12">
         <div className="col-span-1">
           <a href="homepage" className="block">
             Home
@@ -25,7 +25,7 @@ function Footer(props) {
       </div>
       <div className="col-span-2  border-b border-neutral-100 mx-10"></div>
       <div className="col-span-2 mx-10 my-4">
-        <p>2023 Copyright. All Rights Reserve.</p>
+        <p>2023 DigiMaLearn Copyright. All Rights Reserve.</p>
       </div>
     </footer>
   );
