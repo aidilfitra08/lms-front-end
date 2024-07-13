08826b433d2d44d8c5d6b5b306cefe4463d4960b
aidilfitra08 <aidil.gitu1@gmail.com>
2024-07-14 00:54:22 +0700

    new redux state

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -4,11 +4,13 @@ import thunk from "redux-thunk";
 import { userReducer } from "./Credential/UserReducer";
 import { studentReducer } from "./Student/StudentReducer";
 import { createCourseReducer } from "./Lecture/LectureReducer";
+import { courseReducer } from "./Course/CourseReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   student: studentReducer,
   lecture: createCourseReducer,
+  course: courseReducer,
 });
 const Store = configureStore({
   reducer: rootReducer,
