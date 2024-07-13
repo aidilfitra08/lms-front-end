52504920b69750032192edae8d92a81b92c54227
aidilfitra08 <aidil.gitu1@gmail.com>
2024-07-14 00:49:24 +0700

    update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,8 @@ function Footer(props) {
   return (
     <footer
       className={classNames(
-        props.sideBarTrigger ? "pl-64" : "pl-0",
-        " bg-indigo-900 bottom-0 h-52 text-neutral-100"
+        props.sideBarTrigger ? "pl-64 max-md:pl-0" : "pl-0",
+        " bg-slate-800 bottom-0 h-52 text-neutral-100"
       )}
     >
       <div className="grid grid-cols-2 py-6 px-10 mt-12">
@@ -20,7 +20,12 @@ function Footer(props) {
         </div>
         <div className="col-span-1">
           <p className="">Need Help? </p>
-          <p className="">Contact us</p>
+          <a
+            className=""
+            href="https://wa.me/6282388157890?text=Aku%20butuh%20bantuan%20dengan%20aplikasi%20DigiMaLearn"
+          >
+            Contact us
+          </a>
         </div>
       </div>
       <div className="col-span-2  border-b border-neutral-100 mx-10"></div>
