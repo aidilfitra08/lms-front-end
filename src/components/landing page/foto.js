5e6272ce2e7b79aed707ba74681c4e82abcc29ea
aidilfitra08 <aidil.gitu1@gmail.com>
2024-08-05 10:15:48 +0700

    LAST COMMIT

--- src/components/landing page/foto.js
+++ src/components/landing page/foto.js
@@ -2,8 +2,8 @@ import marketingManagement from "../../assets/Landing Page/marketing_management.
 import socialMedia from "../../assets/Landing Page/social_media_marketing.png";
 import copyWriting from "../../assets/Landing Page/copywriting.jpg";
 import facebookAds from "../../assets/Landing Page/facebook_ads.png";
-import youtubeAds from "../../assets/Landing Page/youtube-ads.webp";
-import influencerMarketing from "../../assets/Landing Page/influencer_marketing.jpeg";
+import youtubeAds from "../../assets/Landing Page/youtube-ads.jpg";
+import influencerMarketing from "../../assets/Landing Page/influencer-marketing.jpg";
 import seo from "../../assets/Landing Page/SEO.jpg";
 
 export const thumbnail = [
